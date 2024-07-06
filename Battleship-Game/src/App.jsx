f637ddfda1e772905cdd7d59f517785aeb5b781e
Natalia SM <fu.pro.nat@gmail.com>
2024-07-06 12:56:44 -0400

    tableros listos,ahora voy a hacer los barcos

--- Battleship-Game/src/App.jsx
+++ Battleship-Game/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useState } from 'react'
-import { useNavigate, Route, Routes, Router } from 'react-router-dom'
+//import { useState } from 'react'
+import { useNavigate, Route, Routes } from 'react-router-dom'
 
 import { Header } from './Header.jsx'
 import { Footer } from './Footer.jsx'
@@ -11,6 +11,7 @@ import './App.css'
 
 function App() {
   const navigate = useNavigate();
+
   const startPlay = () => {
     navigate('/board');
   }
@@ -21,7 +22,12 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Play startPlay={startPlay} />} />
-        <Route path="/board" element={<Board />} /> 
+        <Route path="/board" element={
+          <div className='boards-container'>
+          <Board title=<h5>⚓️ Tu tablero de batalla ⚓️</h5>/>
+          <Board title=<h5>🦾 Computador </h5> />
+          </div>
+        } /> 
       </Routes>
       <Footer />
     </div>
--- Battleship-Game/src/Board.jsx
+++ Battleship-Game/src/Board.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import "./Board.css"
 
-export const Board = () => {
+export const Board = ({ title }) => {
+
+    //matriz 10x10 para el tablero
+    const board = Array(10).fill(null).map(() => Array(10).fill(null));
     return (
-        <div>
-            <div className="board-player"> tu tablero</div>
-            <div className="board-computer"> computadora</div>
+        <div className="board">
+            <h1>{title}</h1>
+            <div className="grid">
+                {board.map((row, rowIndex) => (
+                    <div key={rowIndex} className="row">
+                        {row.map((cell, cellIndex) => (
+                            <div key={cellIndex} className="cell">
+                            </div>
+                        ))}
+                    </div>
+                ))}
+            </div>
         </div>
-    
+
     )
 }
